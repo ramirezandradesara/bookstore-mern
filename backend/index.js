f15594e9b10897065cf8f576a1b9b50b080c871b
Sara Ramírez Andrade <sararamirezandrade@gmail.com>
2024-08-05 18:28:48 -0300

    9. Delete a book with mongoose

--- backend/index.js
+++ backend/index.js
@@ -48,7 +48,7 @@ app.get("/books", async (request, response) => {
       data: books,
     });
   } catch (error) {
-    console.log(error);
+    console.log(error.message);
     response.status(500).send({ message: error.message });
   }
 });
@@ -60,7 +60,7 @@ app.get("/books/:id", async (request, response) => {
     const book = await Book.findById(id);
     return response.status(200).json(book);
   } catch (error) {
-    console.log(error);
+    console.log(error.message);
     response.status(500).send({ message: error.message });
   }
 });
@@ -81,7 +81,22 @@ app.put("/books/:id", async (req, res) => {
 
     return res.status(200).send({ message: "Book update sucessfully" });
   } catch (error) {
-    console.log(error);
+    console.log(error.message);
+    res.status(500).send({ message: error.message });
+  }
+});
+
+// Route to delete a book by id
+app.delete("/books/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await Book.findByIdAndDelete(id);
+
+    if (!result) return res.status(404).send({ message: "Book not found" });
+
+    return res.status(200).send({ message: "Book deleted sucessfully" });
+  } catch (error) {
+    console.log(error.message);
     res.status(500).send({ message: error.message });
   }
 });
