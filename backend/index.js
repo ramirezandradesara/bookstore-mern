9668f89502ccc88b1d28efbdaef228d6458dcbbb
Sara Ramírez Andrade <sararamirezandrade@gmail.com>
2024-08-05 18:09:08 -0300

    7. Get One Book by id with mongoose

--- backend/index.js
+++ backend/index.js
@@ -53,6 +53,18 @@ app.get("/books", async (request, response) => {
   }
 });
 
+// Route to get book by id
+app.get("/books/:id", async (request, response) => {
+  try {
+    const { id } = request.params;
+    const book = await Book.findById(id);
+    return response.status(200).json(book);
+  } catch (error) {
+    console.log(error);
+    response.status(500).send({ message: error.message });
+  }
+});
+
 mongoose
   .connect(mongoURL)
   .then(() => {
