089676fa0f7c9930a35c772a37871599ed23504b
Sara Ramírez Andrade <sararamirezandrade@gmail.com>
2024-08-05 17:55:14 -0300

    5. Save a new Book with mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,42 @@
 import express from "express";
 import { PORT, mongoURL } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
 
 const app = express();
+app.use(express.json())
 
 app.get("/", (request, response) => {
   console.log(request);
   return response.status(234).send("Welcome to Bookstore");
+});
+
+// Route to save a new book
+// mongoose create an asynchonous process
+app.post("/books", async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response.status(400).send({
+        message: "Send all required field: title, author and publishYear",
+      });
+    }
+
+    const newBook = {
+      title: request.body.title,
+      author: request.body.author,
+      publishYear: request.body.publishYear,
+    };
+
+    const book = await Book.create(newBook);
+    return response.status(201).send(book);
+  } catch (error) {
+    console.log(error);
+    response.status(500).send({ message: error.message });
+  }
 });
 
 mongoose
