bda65eee4c002a3961798ac89986e117f7f8bcfc
Sara Ramírez Andrade <sararamirezandrade@gmail.com>
2024-08-05 22:55:28 -0300

    11. CORS policy in Node js and Express js

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,34 @@
+import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
-import { Book } from "./models/bookModel.js";
-import dotenv from "dotenv";
-import bookRoute from "./routes/booksRoutes.js"
-const app = express();
-// Middleware for parsing request bosy
-app.use(express.json());
+import bookRoute from "./routes/booksRoutes.js";
+
 dotenv.config();
 const url = process.env.mongoURL;
+const app = express();
+
+// Middleware for parsing request bosy
+app.use(express.json());
+
+// Middleware for handling CORS POLICY
+// Option 1: Allow all origins with default of cors(*)
+app.use(cors());
+
+// Option 2: Allow custom origins => More control
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
 app.get("/", (request, response) => {
   console.log(request);
   return response.status(234).send("Welcome to Bookstore");
 });
 
-app.use("/books", bookRoute)
+app.use("/books", bookRoute);
 
 mongoose
   .connect(url)
